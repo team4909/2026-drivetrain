81e98e6b00c88a74a55d67e19566ca287716fcfe
taj-maharj08 <122415382+taj-maharj08@users.noreply.github.com>
2026-01-17 12:56:53 -0500

    untested 2 motor indexer

--- src/main/java/frc/robot/subsystems/indexer/IndexerIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerIOTalonFX.java
@@ -2,43 +2,60 @@ package frc.robot.subsystems.indexer;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import frc.robot.subsystems.indexer.IndexerIOInputsAutoLogged;
 
 public class IndexerIOTalonFX implements IndexerIO {
 
-    private final TalonFX m_indexermotor;
-    private final int kIndexerMotorID = 22;
+    // Bottom = leader
+    private final TalonFX m_indexerBottomMotor;
+    // Top = follower
+    private final TalonFX m_indexerTopMotor;
+
+    private final int kIndexerBottomMotorID = 22;
+    private final int kIndexerTopMotorID = 23;
+
     private final String kCanbus = "CANivore2";
 
-
     public IndexerIOTalonFX() {
-        m_indexermotor = new TalonFX(kIndexerMotorID, kCanbus);
+        m_indexerBottomMotor = new TalonFX(kIndexerBottomMotorID, kCanbus);
+        m_indexerTopMotor = new TalonFX(kIndexerTopMotorID, kCanbus);
 
         final TalonFXConfiguration indexerMotorConfig = new TalonFXConfiguration();
         indexerMotorConfig.CurrentLimits.SupplyCurrentLimit = 40.0;
         indexerMotorConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
 
-        m_indexermotor.getConfigurator().apply(indexerMotorConfig);
+        m_indexerBottomMotor.getConfigurator().apply(indexerMotorConfig);
+        m_indexerTopMotor.getConfigurator().apply(indexerMotorConfig);
+
+     
+        m_indexerTopMotor.setControl(
+            new Follower(kIndexerBottomMotorID, MotorAlignmentValue.Aligned)
+        );
     }
 
     @Override
     public void setSpeed(double speed) {
-        m_indexermotor.setControl(new DutyCycleOut(speed));
+  
+        m_indexerBottomMotor.setControl(new DutyCycleOut(speed));
     }
 
     @Override
     public void setBrakeMode(boolean enableBrakeMode) {
-        final NeutralModeValue neutralModeValue = enableBrakeMode ? NeutralModeValue.Brake : NeutralModeValue.Coast;
-        m_indexermotor.setNeutralMode(neutralModeValue);
+        final NeutralModeValue neutralModeValue =
+            enableBrakeMode ? NeutralModeValue.Brake : NeutralModeValue.Coast;
+
+        m_indexerBottomMotor.setNeutralMode(neutralModeValue);
+        m_indexerTopMotor.setNeutralMode(neutralModeValue);
     }
 
     public void updateInputs(IndexerIOInputsAutoLogged m_inputs) {
-        m_inputs.speed = m_indexermotor.getVelocity().getValueAsDouble();
-        m_inputs.statorCurrent = m_indexermotor.getStatorCurrent().getValueAsDouble();
-        m_inputs.supplyCurrent = m_indexermotor.getSupplyCurrent().getValueAsDouble();
+        m_inputs.speed = m_indexerBottomMotor.getVelocity().getValueAsDouble();
+        m_inputs.statorCurrent = m_indexerBottomMotor.getStatorCurrent().getValueAsDouble();
+        m_inputs.supplyCurrent = m_indexerBottomMotor.getSupplyCurrent().getValueAsDouble();
     }
-
 }
