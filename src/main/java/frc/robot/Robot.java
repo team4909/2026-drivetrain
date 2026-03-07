16322a266e2c4cb5ae2556a036ea7657930cbfa5
LTeixeira4909 <ipadljteixeira@gmail.com>
2026-03-07 15:03:30 -0500

    add logging

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,7 +7,8 @@ package frc.robot;
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.NT4Publisher;
- 
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -18,6 +19,7 @@ public class Robot extends LoggedRobot {
 
   public Robot() {
     Logger.addDataReceiver(new NT4Publisher());
+    Logger.addDataReceiver(new WPILOGWriter("/home/lvuser/logs"));
     Logger.start();
 
     m_robotContainer = new RobotContainer();
